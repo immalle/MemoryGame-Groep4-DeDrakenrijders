5241e9079ba70f215ffc840deb4641617673b2fa
Stijn Geeraerts <stijn.geeraerts@immalle.be>
2013-03-20 11:59:50 +0100

    update testproject resources

--- resources test/resources test/Form1.vb
+++ resources test/resources test/Form1.vb
@@ -1,3 +1,12 @@
 ﻿Public Class Form1
 
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        For i = 0 To 2
+            Dim x As Integer
+            Dim rnd As System.Random()
+
+            PictureBox1.Image = My.Resources.ResourceManager.GetString(x)
+
+        Next
+    End Sub
 End Class
