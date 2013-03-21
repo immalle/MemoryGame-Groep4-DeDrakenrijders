ddf9e37a0c7b27c7760fa758c38f4bf0b0cfcf63
Stijn Geeraerts <stijn.geeraerts@immalle.be>
2013-03-21 11:48:02 +0100

    update random pictures (2)

--- resources test/resources test/Form1.vb
+++ resources test/resources test/Form1.vb
@@ -6,15 +6,20 @@
         For i = 0 To 9
             Dim a As Integer = randomGenerator.Next(1, 6)
             Dim pict As New Label()
+            pict.Hide()
             pict.Size = New Size(100, 150)
             pict.Location = New Point(150 * i, 30)
             pict.Image = My.Resources.ResourceManager.GetObject("_" & a)
 
             Me.Controls.Add(pict)
 
+            AddHandler pict.MouseClick, AddressOf LabelOnMouseClickEventHandler
+
 
         Next
-
+    End Sub
+    Private Sub LabelOnMouseClickEventHandler(sender As Label, e As System.EventArgs)
+        sender()
 
     End Sub
 End Class
