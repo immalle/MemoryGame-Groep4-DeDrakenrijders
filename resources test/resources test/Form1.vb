1949c970f8c10f7ef5ea0c51d8e6f7d3ee152411
Stijn Geeraerts <stijn.geeraerts@immalle.be>
2013-03-21 11:23:32 +0100

    update random pictures

--- resources test/resources test/Form1.vb
+++ resources test/resources test/Form1.vb
@@ -2,9 +2,19 @@
 
     Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
         Dim randomGenerator As New Random()
-        For i = 0 To 2
-            Dim x As Integer = randomGenerator.Next(5)
-            PictureBox1.Image = My.Resources.ResourceManager.GetObject("_" & x)
+        
+        For i = 0 To 9
+            Dim a As Integer = randomGenerator.Next(1, 6)
+            Dim pict As New Label()
+            pict.Size = New Size(100, 150)
+            pict.Location = New Point(150 * i, 30)
+            pict.Image = My.Resources.ResourceManager.GetObject("_" & a)
+
+            Me.Controls.Add(pict)
+
+
         Next
+
+
     End Sub
 End Class
